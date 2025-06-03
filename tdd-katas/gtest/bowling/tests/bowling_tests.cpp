f9c6826c8a7c07bd26d7d4ebc369f662fa5f82c3
Krystian Piękoś <krystian.piekos@infotraining.pl>
2025-06-03 10:13:08 +0200

    BowlingGame - All rolls in gutter

--- tdd-katas/gtest/bowling/tests/bowling_tests.cpp
+++ tdd-katas/gtest/bowling/tests/bowling_tests.cpp
@@ -9,7 +9,35 @@
 
 using namespace std;
 
-TEST(Example, ExampleTest)
+class BowlingGame
 {
-    EXPECT_EQ(1, 1);
+public:
+    void roll(uint32_t)
+    {        
+    }
+
+    uint32_t score() const
+    {
+        return 0;
+    }
+};
+
+TEST(BowlingGameTests, WhenGameStarts_ScoreReturnsZero)
+{
+    BowlingGame game;
+
+    ASSERT_EQ(game.score(), 0);
+}
+
+TEST(BowlingGameTests, WhenAllRollsInGutter_ScoreReturnsZero)
+{
+    BowlingGame game;
+    
+    const int MAX_ROLLS = 20;
+    for(int i = 0; i < MAX_ROLLS; ++i)
+    {
+        game.roll(0);
+    }
+
+    ASSERT_EQ(game.score(), 0);
 }
